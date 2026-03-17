4d9dd40f1a99ad9db7af6589ad0dc781f7989048
vbaswin <aswinvb10a@gmail.com>
2026-03-18 00:54:41 +0530

    feat(ui): button for auto-register added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include <QHBoxLayout>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,6 +13,7 @@ MainWindow::~MainWindow() {}
 
 void MainWindow::setupUI()
 {
+    m_container = new QWidget(this);
     this->setCentralWidget(m_container);
     QHBoxLayout *hLayout = new QHBoxLayout(m_container);
 
@@ -19,6 +21,12 @@ void MainWindow::setupUI()
     m_rightControlsWidget = new QWidget(m_container);
     m_rightControlsWidget->setObjectName("rightControls");
 
-    hLayout->addWidget(m_vtkWidget);
-    hLayout->addWidget(m_rightControlsWidget);
+    hLayout->addWidget(m_vtkWidget, 6);
+    hLayout->addWidget(m_rightControlsWidget, 1);
+
+    QPushButton *autoRegisterBtn = new QPushButton("Auto Register", m_rightControlsWidget);
+    autoRegisterBtn->setCheckable(true);
+    // autoRegisterBtn->
+    QVBoxLayout *vLayout = new QVBoxLayout(m_rightControlsWidget);
+    vLayout->addWidget(autoRegisterBtn);
 }
